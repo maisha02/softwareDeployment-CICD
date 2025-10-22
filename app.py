d14312b4b86104677f5cf54561fdb4bfc05f173a
Yaser Haidari <104608220@student.swin.edu.au>
2025-10-22 15:43:45 +1100

    testing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def create_app():
 
     @app.route('/')
     def index():
-        return jsonify({"message": "Hello from aaaa!"}), 200
+        return jsonify({"message": "Hello from love!"}), 200
 
     @app.route('/health')
     def health():
